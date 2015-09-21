f7cc1aeda3b92499341876a814f30637fde34cde
songzhiming <15117951262@163.com>
2015-09-21 17:44:18 +0800

    初始化项目

---
+++ BaseWebViewDemo/BaseWebViewDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BaseWebViewDemo
+//
+//  Created by 宋志明 on 15/9/21.
+//  Copyright (c) 2015年 宋志明. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BaseWebViewDemo/BaseWebViewDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BaseWebViewDemo
+//
+//  Created by 宋志明 on 15/9/21.
+//  Copyright (c) 2015年 宋志明. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
